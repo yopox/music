4d6b4d5cb2314a597317ae4b4358a0df643393b8
yopox <yopoxdev@gmail.com>
2017-12-27 17:30:00 +0100

    Removed parallax ;p

--- index.php
+++ index.php
@@ -6,32 +6,23 @@
 <head>
     <meta charset="utf-8">
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="master.css">
+    <link rel="stylesheet" href="master.min.css">
     <meta name="viewport" content="width=device-width, user-scalable=no">
 </head>
 
 <body class=<?php echo '"' . $class . '"' ?>>
 
-    <ul id="scene" class="scene">
-        <li class="layer" data-depth="0.1">
-            <div id="bg" class=<?php echo '"' . $class . '"' ?>><?php echo $title; ?></div>
-        </li>
-        <li class="layer" data-depth="0.2">
-            <div id="playlist">
-                <table>
-                    <?php include("parse.php"); ?>
-                </table>
-            </div>
-        </li>
-    </ul>
-
+    <div id="bg" class=<?php echo '"' . $class . '"' ?>><?php echo $title; ?></div>
+    
+    <div id="playlist">
+        <table>
+            <?php include("parse.php"); ?>
+        </table>
+    </div>
+        
 
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/parallax/deploy/jquery.parallax.js"></script>
     <script type="text/javascript">
-        $(function() {
-            $('#scene').parallax();
-        });
 
         $('tr').click(function() {
             if (this.innerText) {
